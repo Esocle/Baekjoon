7564f16761a33dc5598c2aa4984e5323cfa7914b
Esocle <91296140+Esocle@users.noreply.github.com>
2022-09-29 09:52:00 +0900

    [Bronze V] Title: 사분면 고르기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,12 +1,6 @@
-A = int(input())
-B = int(input())
-if A * B > 0:
-    if A > 0:
-        print("1")
-    else:
-        print("3")
+x = int(input())
+y = int(input())
+if x > 0:
+    print(1 if y > 0 else 4)
 else:
-    if A > 0:
-        print("4")
-    else:
-        print("2")
+    print(2 if y > 0 else 3)
