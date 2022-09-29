85865acbd24bbc63695e92e7ac65bf2cb9194f88
Esocle <91296140+Esocle@users.noreply.github.com>
2022-09-29 09:49:25 +0900

    [Bronze V] Title: 사분면 고르기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,12 @@
+A = int(input())
+B = int(input())
+if A * B > 0:
+    if A > 0:
+        print("1")
+    else:
+        print("3")
+else:
+    if A > 0:
+        print("4")
+    else:
+        print("2")
