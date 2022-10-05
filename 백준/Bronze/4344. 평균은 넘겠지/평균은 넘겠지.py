3fa061374056030dd0226558862ab4fd6393ea05
Esocle <91296140+Esocle@users.noreply.github.com>
2022-10-05 14:13:42 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -2,6 +2,8 @@ temp = int(input())
 for _ in range(temp):
     l = list(map(int, input().split(' ')))
     avg = (sum(l[1:])) / l[0]
-    avg_over = [e for e in l[1:] if e > avg]
-    rate = len(avg_over) / l[0] * 100
-    print(f'{rate:.3f}%')
+    cnt = 0
+    for e in l[1:]:
+        if e > avg:
+            cnt +=1
+    print(f'{cnt/l[0]*100:.3f}%')
