fb45b737bb873d5795fccc8d066fa23ce0357ab8
Esocle <91296140+Esocle@users.noreply.github.com>
2022-10-11 09:19:26 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,11 @@
+n = int(input())
+cnt = n
+for _ in range(n):
+    word = input()
+    for i in range(len(word)-1):
+        if word[i] == word[i+1]:
+            pass
+        elif word[i] in word[i+1:]:
+            cnt -= 1
+            break
+print(cnt)
