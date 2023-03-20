820438413c81326648766229e9fd9523e1792953
Bae Seohyeong <shbae1207@gmail.com>
2023-03-20 10:38:28 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -5,7 +5,5 @@ n, m = map(int, input().split())
 ball = list(range(1, n+1))
 for _ in range(m):
     a, b = map(int, input().split())
-    temp = ball[a-1]
-    ball[a-1] = ball[b-1]
-    ball[b-1] = temp
+    ball[a-1], ball[b-1] = ball[b-1], ball[a-1]
 print(*ball)
