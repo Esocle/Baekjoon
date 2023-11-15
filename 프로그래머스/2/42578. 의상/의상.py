b0952c4f4588d0d80ab02733d06a6b7429a913b7
Bae Seohyeong <shbae1207@gmail.com>
2023-11-15 17:36:19 +0900

    Delete 프로그래머스/2/42578. 의상 directory

--- 프로그래머스/2/42578. 의상/의상.py
+++
@@ -1,9 +0,0 @@
-def solution(clothes):
-    hash_map = {}
-    for c in clothes:
-        hash_map[c[1]] = hash_map.get(c[1], 0) + 1
-    print(hash_map)
-    answer = 1
-    for h in hash_map:
-        answer *= (hash_map[h]+1)
-    return answer - 1
